a65d1e4ceccd93b4ade7daec0153306b63f3bd3c
alia <alia@blackfoot.io>
2026-06-12 12:19:28 +0200

    Add model evaluation

---
+++ src/evaluate.py
@@ -0,0 +1,51 @@
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import ConfusionMatrixDisplay
+
+def plot_history(history):
+
+    plt.figure(figsize=(8,5))
+
+    plt.plot(history.history["accuracy"], label="train")
+    plt.plot(history.history["val_accuracy"], label="val")
+
+    plt.xlabel("Epoch")
+    plt.ylabel("Accuracy")
+    plt.title("Training Accuracy")
+
+    plt.legend()
+    plt.savefig("images/training_curve.png")
+    plt.show()
+
+
+def plot_confusion_matrix(model, test_ds, class_names):
+
+    y_true = []
+    y_pred = []
+
+    for images, labels in test_ds:
+
+        predictions = model.predict(images, verbose=0)
+
+        predictions = (predictions > 0.5).astype(int).flatten()
+
+        y_true.extend(labels.numpy())
+        y_pred.extend(predictions)
+
+    cm = confusion_matrix(y_true, y_pred)
+
+    disp = ConfusionMatrixDisplay(
+        confusion_matrix=cm,
+        display_labels=class_names
+    )
+
+    fig, ax = plt.subplots(figsize=(6,6))
+
+    disp.plot(ax=ax)
+
+    plt.title("Confusion Matrix")
+
+    plt.savefig("images/confusion_matrix.png")
+
+    plt.show()
+
--- src/train.py
+++ src/train.py
@@ -1,6 +1,7 @@
-from data.load_data import load_datasets
-from models.cnn_model import build_model
-from utils.load_config import load_config
+from src.data.load_data import load_datasets
+from src.models.cnn_model import build_model
+from src.utils.load_config import load_config
+from src.evaluate import plot_history, plot_confusion_matrix
 
 config = load_config()
 
@@ -14,4 +15,16 @@ history = model.fit(
     train_ds,
     validation_data=val_ds,
     epochs=config["training"]["epochs"]
+)
+
+model.save("saved_models/mobilenet_baseline.keras")
+
+test_loss, test_acc = model.evaluate(test_ds)
+
+print("Test accuracy:", test_acc)
+plot_history(history)
+plot_confusion_matrix(
+    model,
+    test_ds,
+    class_names
 )
