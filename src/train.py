ec51ffa33a590e3bc987943280053422eab017f1
alia <alia@blackfoot.io>
2026-06-12 13:59:28 +0200

    Add Earlystopping and ModelCheckPoint

--- src/train.py
+++ src/train.py
@@ -1,3 +1,4 @@
+from tensorflow.keras import callbacks
 from src.data.load_data import load_datasets
 from src.models.cnn_model import build_model
 from src.utils.load_config import load_config
@@ -18,11 +19,25 @@ model = build_model(config)
 
 model.summary()
 
+training_callbacks = [
+    callbacks.EarlyStopping(
+        monitor="val_loss",
+        patience=config["training"]["patience"],
+        restore_best_weights=True
+    ),
+    callbacks.ModelCheckpoint(
+        filepath="saved_models/best_model.keras",
+        monitor="val_loss",
+        save_best_only=True
+    )
+]
+
 history = model.fit(
     train_ds,
     validation_data=val_ds,
     epochs=config["training"]["epochs"],
-    class_weight=class_weights
+    class_weight=class_weights,
+    callbacks=training_callbacks
 )
 
 model.save("saved_models/mobilenet_baseline.keras")
